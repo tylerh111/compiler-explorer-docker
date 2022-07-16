5ad5e573ab7cc6d90b28981597b5013250ef97dd
Tyler Hughes <tylerh.code@gmail.com>
2022-07-15 21:11:11 -0400

    backup 2: actually works

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 #   Arguments:
 #       - [revision] : optional, specify which revision to use when cloning compiler-explorer
 #           It is advised to use a commit hash or tag as this is used in tagging the docker image
-cd $(dirname $0)
+cd $(dirname $0)/../
 
 ce_commit=main
 if [ ! -z $1 ]; then
@@ -13,6 +13,6 @@ if [ ! -z $1 ]; then
 fi
 
 echo "Building compiler-explorer:$ce_commit"
-docker build -t compiler-explorer:latest --build-arg commit=$ce_commit .
+docker build -f compiler-explorer-docker/Dockerfile -t compiler-explorer:latest --build-arg commit=$ce_commit .
 docker tag compiler-explorer:latest compiler-explorer:$ce_commit
 echo "Successfully tagged compiler-explorer:$ce_commit"
